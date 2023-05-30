ce396f980d524dc3b6c1b37fcfb35738d3e05829
nerbrain <malikmox@gmail.com>
2023-05-30 14:57:57 +0000

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ Routes:
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
@@ -39,8 +39,8 @@ def countAll():
         - users: Count of "User" instances.
     """
     return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+            cities=storage.count("City"),
+            places=storage.count("Place"),
+            reviews=storage.count("Review"),
+            states=storage.count("State"),
+            users=storage.count("User"))
