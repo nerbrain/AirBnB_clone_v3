71ecf0e7dc7f04029b67c0647ed4a60684c83cda
nerbrain <malikmox@gmail.com>
2023-05-30 10:22:31 +0000

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+""" Index File"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
